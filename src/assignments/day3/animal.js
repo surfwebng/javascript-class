354f3cc33f09b0616b2042ed0f7d7be53d0f547d
surfwebng <semaj@surfweb.com.ng>
2018-06-19 18:15:27 +0100

    day2 assignment update

---
+++ src/assignments/day3/animal.js
@@ -0,0 +1,18 @@
+class Cat extends Animal {
+public breed = ‘’;
+
+public isAMamma1(){
+
+public meow()
+}
+
+var dog = {
+
+name: “Bingo”,
+age : 3,
+job: “Security”,
+
+sayName: function(){
+alert(this,name);
+}
+};
